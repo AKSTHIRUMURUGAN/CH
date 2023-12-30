341e734df5499eacf10b42fea2f09322e2d656cb
AKSTHIRUMURUGAN <85742080+AKSTHIRUMURUGAN@users.noreply.github.com>
2023-12-30 17:01:04 +0530

    Delete clubhubrecfinal-main directory

--- clubhubrecfinal-main/clubhub/src/redux/store.jsx
+++
@@ -1,9 +0,0 @@
-import  {configureStore} from "@reduxjs/toolkit"
-import eventReducers from "./eventSlice"
-const store = configureStore({
-    reducer:{
-        events:eventReducers
-
-    }
-})
-export default store;
